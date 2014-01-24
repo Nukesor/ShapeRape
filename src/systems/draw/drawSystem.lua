4cc0ba61cd8277763a46e230be265e9301aa53a6
Arne Beer <arne@twobeer.de>
2014-01-25 00:03:40 +0100

    Random stuff fixed, Drawsystem and DrawableComponent refactored

--- src/systems/draw/drawSystem.lua
+++ src/systems/draw/drawSystem.lua
@@ -1,9 +1,16 @@
 DrawSystem = class("DrawSystem", System)
 
+function DrawSystem:__init()
+end
+
 function DrawSystem:draw()
+    love.graphics.setColor(255, 255, 255, 255)
     for index, entity in pairs(self.targets) do
-        local position = entity:getComponent("PositionComponent")
-        love.graphics.draw(entity:getComponent("DrawableComponent").image, position.x, position.y)
+        local drawable = entity:getComponent("DrawableComponent")
+        local pos = entity:getComponent("PositionComponent")
+
+        -- Draws the Picture. If Entity is near to the beginng or the end of the screen, the Entity is drawed on both sides for sideChangeSystem animation.
+        love.graphics.draw(drawable.image, pos.x, pos.y, drawable.r, drawable.sx, drawable.sy, drawable.ox, drawable.oy)
     end
 end
 
