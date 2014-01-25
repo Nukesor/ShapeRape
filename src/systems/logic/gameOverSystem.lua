b5da1526c7aa757a478e5e27cdc847cd48c26d28
Arne Beer <arne@twobeer.de>
2014-01-25 17:39:35 +0100

    ActionBar working again

--- src/systems/logic/gameOverSystem.lua
+++ src/systems/logic/gameOverSystem.lua
@@ -1,7 +1,7 @@
 GameOverSystem = class("GameOverSystem", System)
 
 function GameOverSystem:update(dt)
-    stack:current().actionBar = stack:current().actionBar
+    stack:current().actionBar = stack:current().actionBar - dt*10
     if stack:current().actionBar < 0 then
         save:saveHighscore(stack:current().score)
         stack:push(GameOverState())
