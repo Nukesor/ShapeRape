fff4f9f2054cfcaa2d2637d451930048fea0572b
Rafael Epplee <raffomania@gmail.com>
2014-01-25 18:23:16 +0100

    Fixed RandomRotationSystem.. again

--- src/systems/logic/randomRotationSystem.lua
+++ src/systems/logic/randomRotationSystem.lua
@@ -4,7 +4,12 @@ function RandomRotationSystem:update(dt)
 	for index, entity in pairs(self.targets) do
 		if entity:getComponent("AnimateComponent") == nil then
 			drawable = entity:getComponent("DrawableComponent")
-			local rotation = math.ceil(love.math.random(-1,1))
+			local rotation = love.math.random(0, 2)
+			if rotation >= 1 then
+				rotation = 1
+			else
+				rotation = -1
+			end
 			local time = 1 + love.math.random(1,2)
 			local targetR = drawable.r + (love.math.random(1,3))*rotation
 			local anim = AnimateComponent(time, drawable, {r = targetR}, "inOutCubic")
