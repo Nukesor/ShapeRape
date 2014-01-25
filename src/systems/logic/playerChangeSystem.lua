10de9548e72063e02765ec4c89cf8f6806209ef3
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2014-01-25 16:14:03 +0100

    player now changes shape

--- src/systems/logic/playerChangeSystem.lua
+++ src/systems/logic/playerChangeSystem.lua
@@ -2,4 +2,20 @@ PlayerChangeSystem = class("PlayerChangeSystem", System)
 
 function PlayerChangeSystem:playerMoved(event)
     
+    local player = table.firstElement(stack:current().engine:getEntityList("PlayerNodeComponent"))
+    currentShape = player:getComponent("ShapeComponent").shape
+    
+    if player:getComponent("PlayerChangeCountComponent").count > 9 then
+        player:getComponent("PlayerChangeCountComponent").count = 0
+        
+        if  currentShape == "circle" then
+            nextShape = "square"
+        elseif currentShape == "square" then
+            nextShape = "triangle"
+        else
+            nextShape = "circle"
+        end
+        player:getComponent("ShapeComponent").shape = nextShape
+        player:getComponent("DrawableComponent").image = resources.images[nextShape]
+    end  
 end
