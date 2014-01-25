128c9b6ff4752c3463e328de8ed02ea36c490f6c
Rafael Epplee <raffomania@gmail.com>
2014-01-25 17:45:51 +0100

    Fixed gameoverstate

--- src/states/gameOverState.lua
+++ src/states/gameOverState.lua
@@ -10,11 +10,21 @@ function GameOverState:load()
                         {"Exit Game", function() love.event.quit() end}}
     self.index = 1
 
-    self.engine:addSystem(MenuPointDisplaySystem(), "draw", 1)
+    local screenWidth = love.graphics.getWidth()
+    for index, item in pairs(self.menupoints) do
+        item.x = (screenWidth/3)
+        item.y = -100
+        item.targetX = item.x
+        item.targetY = 200 + (100 * index)
+    end
+    
+    local menuPointDisplaySystem = MenuPointDisplaySystem()
+    self.engine:addSystem(menuPointDisplaySystem, "draw", 1)
+    self.engine:addSystem(menuPointDisplaySystem, "logic", 1)
 end
 
-function GameOverState:update()
-    self.engine:update()
+function GameOverState:update(dt)
+    self.engine:update(dt)
 end
 
 function GameOverState:draw()
