49746d3cc3fdf12076452a0908e8e7f6fcafef51
C-gotoh <shigotoh1@gmail.com>
2014-01-25 21:03:29 +0100

    no more rotationjumps

--- src/systems/draw/drawSystem.lua
+++ src/systems/draw/drawSystem.lua
@@ -9,16 +9,6 @@ function DrawSystem:draw()
     local nodeWidth = stack:current().nodeWidth
     local middleoffset = nodeWidth/2
 
-    local rotateflag = 0
-    local player
-    local intensity = 1
-    for index, entity in pairs(self.targets) do
-        if entity:getComponent("PlayerNodeComponent") then
-            player = entity
-            intensity = player:getComponent("PlayerChangeCountComponent").count*0.1+0.1
-        end 
-    end
-
     for index, entity in pairs(self.targets) do
         local drawable = entity:getComponent("DrawableComponent")
         local pos = entity:getComponent("PositionComponent")
@@ -28,8 +18,6 @@ function DrawSystem:draw()
         local offsetX = (drawable.image:getWidth())/2
         local offsetY = (drawable.image:getHeight())/2
 
-        drawable.r = drawable.r + rotateflag
-
         local color
         if entity:getComponent("ColorComponent") then
             local color = entity:getComponent("ColorComponent")
@@ -37,7 +25,7 @@ function DrawSystem:draw()
         end
 
         -- Draws the Picture. If Entity is near to the beginng or the end of the screen, the Entity is drawed on both sides for sideChangeSystem animation.
-        love.graphics.draw(drawable.image, posXnew, posYnew, drawable.r*intensity, drawable.sx, drawable.sy, offsetX, offsetY)
+        love.graphics.draw(drawable.image, posXnew, posYnew, drawable.r, drawable.sx, drawable.sy, offsetX, offsetY)
 -- VERSION VON SVEN         
 --        	color = entity:getComponent("ColorComponent")
 --        	love.graphics.setColor(color.r, color.g, color.b)
--- src/systems/logic/randomRotationSystem.lua
+++ src/systems/logic/randomRotationSystem.lua
@@ -1,6 +1,14 @@
 RandomRotationSystem = class("RandomRotationSystem", System)
 
 function RandomRotationSystem:update(dt)
+	local player
+    local intensity = 1
+    for index, entity in pairs(self.targets) do
+        if entity:getComponent("PlayerNodeComponent") then
+            player = entity
+            intensity = player:getComponent("PlayerChangeCountComponent").count*0.1+0.1
+        end 
+    end
 	for index, entity in pairs(self.targets) do
 		if entity:getComponent("AnimateComponent") == nil then
 			drawable = entity:getComponent("DrawableComponent")
@@ -11,7 +19,7 @@ function RandomRotationSystem:update(dt)
 				rotation = -1
 			end
 			local time = 1 + love.math.random(1,2)
-			local targetR = drawable.r + (love.math.random(1,3))*rotation
+			local targetR = drawable.r + (love.math.random(1,3))*(rotation*intensity)
 			local anim = AnimateComponent(time, drawable, {r = targetR}, "inOutCubic")
 			entity:addComponent(anim)
 		end
