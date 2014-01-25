a341b1afddef171f13b4e21ca447084e8ea0115c
C-gotoh <shigotoh1@gmail.com>
2014-01-25 18:24:14 +0100

    sum rotatation

--- src/systems/draw/drawSystem.lua
+++ src/systems/draw/drawSystem.lua
@@ -4,11 +4,20 @@ function DrawSystem:__init()
 end
 
 function DrawSystem:draw()
+    local player 
     love.graphics.setColor(255, 255, 255, 255)
     local nodeWidth = stack:current().nodeWidth
     local middleoffset = nodeWidth/2
 
     local rotateflag = 0
+    local player
+    local intensity = 1
+    for index, entity in pairs(self.targets) do
+        if entity:getComponent("PlayerNodeComponent") then
+            player = entity
+            intensity = player:getComponent("PlayerChangeCountComponent").count
+        end
+    end
 
     for index, entity in pairs(self.targets) do
         local drawable = entity:getComponent("DrawableComponent")
@@ -22,12 +31,12 @@ function DrawSystem:draw()
         drawable.r = drawable.r + rotateflag
 
         if entity:getComponent("ColorComponent") then
-        	local color = entity:getComponent("ColorComponent")
-        	love.graphics.setColor(color.r, color.g, color.b)
+            local color = entity:getComponent("ColorComponent")
+            love.graphics.setColor(color.r, color.g, color.b)
         end
 
         -- Draws the Picture. If Entity is near to the beginng or the end of the screen, the Entity is drawed on both sides for sideChangeSystem animation.
-        love.graphics.draw(drawable.image, posXnew, posYnew, drawable.r, drawable.sx, drawable.sy, offsetX, offsetY)
+        love.graphics.draw(drawable.image, posXnew, posYnew, intensity, drawable.sx, drawable.sy, offsetX, offsetY)
     end
 end
 
