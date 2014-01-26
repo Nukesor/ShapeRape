3dd16ace774b9e2fd8487d9acc099d8c8ac01793
C-gotoh <shigotoh1@gmail.com>
2014-01-26 13:41:39 +0100

    added an achievementstate

---
+++ src/states/achievementState.lua
@@ -0,0 +1,13 @@
+tween = require("lib/tween/tween")
+
+require("states/pauseState")
+
+AchievementState = class("AchievementState", State)
+
+function AchievementState:__init()
+
+end
+
+function AchievementState:load()
+
+end
