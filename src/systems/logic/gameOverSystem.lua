23edef2a3e71f334721675e430f326ad984ef866
Rafael Epplee <raffomania@gmail.com>
2014-01-28 18:31:02 +0100

    Increased difficulty, actionBar now decreases slower in slowmotion

--- src/systems/logic/gameOverSystem.lua
+++ src/systems/logic/gameOverSystem.lua
@@ -8,7 +8,7 @@ function GameOverSystem:update(dt)
     local player = table.firstElement(self.targets)
     if not player then
         self.elapsedTime = self.elapsedTime + dt*0.1
-        stack:current().actionBar = stack:current().actionBar - (self.elapsedTime/100)
+        stack:current().actionBar = stack:current().actionBar - dt*(self.elapsedTime)
     end
     if stack:current().actionBar < 0 then
         save:saveHighscore(stack:current().score)
